410d382bb3a074c9ca92e3e467d223b545a23083
sajad <sajadkamali017@gmail.com>
2023-05-18 01:48:52 +0330

    feat: task completed.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Traffic Vehicle Counter
+
+## About
+
+This code is a Python implementation of a traffic vehicle counter that uses the YOLOv8 object detection model. It can be used to count the number of cars and motorcycles passing through a specific zone of a road, as captured by a video camera.
+
+## Installation
+
+Before running the code, you need to install the following libraries:
+
+- cv2
+- ultralytics
+
+You can install these packages using pip:
+
+```
+pip install opencv ultralytics
+```
+
+## Usage
+
+To use the traffic vehicle counter, first create an instance of the TrafficVehicelCounter class. Then, call the vehicle_detector method to start the vehicle detection process. The vehicle_detector method reads frames from a video file, detects vehicles using YOLOv8, and counts the number of cars and motorcycles passing through a specific zone of the road. 
+
+The script will display the video stream with bounding boxes around each detected vehicle and the count of cars and motorcycles in real-time.
+
+Note: The script uses a pre-trained YOLO model provided in the "models/yolov8n" directory. You can replace the model with your own if you wish.
+
+## Example
+
+![sample](https://github.com/sajiniho07/second_derivative/blob/master/res/TrafficVehicleCounter.gif)
+
+## License 
+
+Made with :heart: by <a href="https://github.com/sajiniho07" target="_blank">Sajad Kamali</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- project.py
+++ project.py
@@ -5,12 +5,16 @@ class TrafficVehicelCounter():
 
     def __init__(self):
         self.detector_zone = [[20, 150], [500, 350]]
-        self.sensitive_zone = [240, 260]
+        self.sensitive_zone = [245, 260]
+        self.x_pos_limit = 7
+        self.car_id = 2
+        self.motor_id = 3
         self.model = YOLO("models/yolov8n")
      
     def vehicle_detector(self):
         motor_cnt = 0
         car_cnt = 0
+        vehicle_lst = [[1, 1]]
         cap = cv2.VideoCapture("res/roud.mp4")
         while True:
             ret, frame = cap.read()
@@ -21,36 +25,52 @@ class TrafficVehicelCounter():
                 ltl_frame = frame[self.detector_zone[0][1]:self.detector_zone[1][1], 
                                   self.detector_zone[0][0]:self.detector_zone[1][0]]
                 
-                car_cnt_title = f"Car count: {car_cnt}"
-                BLACK = (0, 0, 0)
-                cv2.putText(frame, car_cnt_title, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, BLACK, 2)
-                
-                motor_cnt_title = f"Motor count: {motor_cnt}"
-                BLACK = (0, 0, 0)
-                cv2.putText(frame, motor_cnt_title, (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 1, BLACK, 2)
+                self.set_label(motor_cnt, car_cnt, frame)
 
                 results = self.model(source=ltl_frame)
                 result = results[0]
                 for (obj_xyxy, obj_cls) in zip(result.boxes.xyxy, result.boxes.cls):
                     obj_cls = int(obj_cls)
-                    x1 = int(obj_xyxy[0].item()) + self.detector_zone[0][0]
-                    y1 = int(obj_xyxy[1].item()) + self.detector_zone[0][1]
-                    x2 = int(obj_xyxy[2].item()) + self.detector_zone[0][0]
-                    y2 = int(obj_xyxy[3].item()) + self.detector_zone[0][1]
+                    x1, y1, x2, y2 = self.get_coordinates(obj_xyxy)
+                    x_cent, y_cent = self.get_center(x1, y1, x2, y2)
+
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 1)
-                    # x_cent = (x2 + x1) / 2
-                    y_cent = int((y2 + y1) / 2)
                     cv2.rectangle(frame, (self.detector_zone[0][0], self.sensitive_zone[0]), 
                                 (self.detector_zone[1][0], self.sensitive_zone[1]), (0, 0, 255), 1)
+                    
                     if self.sensitive_zone[0] < y_cent < self.sensitive_zone[1]:
-                        if obj_cls == 2:
-                            car_cnt = car_cnt + 1
-                        elif obj_cls == 3:
-                            motor_cnt = motor_cnt + 1
+                        if abs(vehicle_lst[-1][0] - x_cent) > self.x_pos_limit or vehicle_lst[-1][1] != obj_cls:
+                            if obj_cls == self.car_id:
+                                car_cnt = car_cnt + 1
+                                vehicle_lst.append([x_cent, obj_cls])
+                            elif obj_cls == self.motor_id:
+                                motor_cnt = motor_cnt + 1
+                                vehicle_lst.append([x_cent, obj_cls])
                 cv2.imshow("Video", frame)
                 q = cv2.waitKey(1)
                 if q == ord('q'):
                     break
 
+    def set_label(self, motor_cnt, car_cnt, frame):
+        car_cnt_title = f"Cars count: {car_cnt}"
+        COLOR = (0, 0, 255)
+        cv2.putText(frame, car_cnt_title, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR, 2)
+                
+        motor_cnt_title = f"Motors count: {motor_cnt}"
+        COLOR = (0, 0, 255)
+        cv2.putText(frame, motor_cnt_title, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, COLOR, 2)
+
+    def get_coordinates(self, obj_xyxy):
+        x1 = int(obj_xyxy[0].item()) + self.detector_zone[0][0]
+        y1 = int(obj_xyxy[1].item()) + self.detector_zone[0][1]
+        x2 = int(obj_xyxy[2].item()) + self.detector_zone[0][0]
+        y2 = int(obj_xyxy[3].item()) + self.detector_zone[0][1]
+        return x1, y1, x2, y2
+    
+    def get_center(self, x1, y1, x2, y2):
+        x_cent = int((x2 + x1) / 2)
+        y_cent = int((y2 + y1) / 2)
+        return x_cent, y_cent
+    
 trafficVehicelCounter = TrafficVehicelCounter()
 trafficVehicelCounter.vehicle_detector()
